518d356a6996aa820a1e631401fa9c91901afa0e
20018040 <Jeff.kim01@student.csulb.edu>
2025-12-19 21:07:03 -0800

    background

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,26 @@ import Navbar from "./components/Navbar.tsx";
 import "bootstrap/dist/css/bootstrap.css";
 import Message from "./components/Message.tsx";
 import "./App.css";
+// import Particles from './Particles';
+import Background from "./components/Background.tsx";
+import Particles from "./components/Particles.tsx";
 function App() {
   let navBars = ["Home", "Education", "Projects", "Contact"];
-  const homeMessage = "Hello, Thank you for stopping by my portfolio.";
+  const homeMessage = "Hellso, Thank you for stopping by my portfolio.";
   return (
+    <>
+      <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+  <Particles
+    particleColors={['#ffffff', '#ffffff']}
+    particleCount={200}
+    particleSpread={10}
+    speed={0.1}
+    particleBaseSize={100}
+    moveParticlesOnHover={true}
+    alphaParticles={false}
+    disableRotation={false}
+  />
+</div>
     <div
       style={{
         paddingTop: 2,
@@ -26,6 +42,7 @@ function App() {
         </div>
       </div>
     </div>
+    </>
   );
 }
 
