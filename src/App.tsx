348067691863dfef00310f69c969d6b84dc1460d
20018040 <Jeff.kim01@student.csulb.edu>
2025-12-20 22:34:36 -0800

    Backgroudn correctly renders with navbar and message

--- src/App.tsx
+++ src/App.tsx
@@ -7,42 +7,57 @@ import Background from "./components/Background.tsx";
 import Particles from "./components/Particles.tsx";
 function App() {
   let navBars = ["Home", "Education", "Projects", "Contact"];
-  const homeMessage = "Hellso, Thank you for stopping by my portfolio.";
+  const homeMessage = "Hello, Thank you for stopping by my portfolio.";
+
   return (
-    <>
-      <div style={{ width: '100%', height: '600px', position: 'relative' }}>
-  <Particles
-    particleColors={['#ffffff', '#ffffff']}
-    particleCount={200}
-    particleSpread={10}
-    speed={0.1}
-    particleBaseSize={100}
-    moveParticlesOnHover={true}
-    alphaParticles={false}
-    disableRotation={false}
-  />
-</div>
     <div
       style={{
-        paddingTop: 2,
-        backgroundColor: "#ffffffff",
-        color: "#9492eeff",
+        position: "relative",
+        width: "100%",
+        height: "100vh",
+        overflow: "hidden",
       }}
     >
-      <div>
-        <Navbar names={navBars}></Navbar>
-        <div
-          style={{
-            marginTop: 120,
-            marginLeft: 64,
-          }}
-        >
+      {/* Particles background wrapped in a div */}
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          zIndex: 0,
+        }}
+      >
+        <Particles
+          particleColors={["#ffffff", "#ffffff"]}
+          particleCount={200}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
+        />
+      </div>
+
+      {/* Content on top */}
+      <div
+        style={{
+          position: "relative",
+          zIndex: 1,
+          paddingTop: 20,
+          backgroundColor: "transparent",
+          color: "#9492eeff",
+        }}
+      >
+        <Navbar names={navBars} />
+        <div style={{ marginTop: 120, marginLeft: 64 }}>
           <Message welcome={homeMessage} size={36} />
-          <Message welcome="My Name is Jeff Kim" size={60}></Message>
+          <Message welcome="My Name is Jeff Kim" size={60} />
         </div>
       </div>
     </div>
-    </>
   );
 }
 
