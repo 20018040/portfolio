8c51c11130a2104bfe5a6a5d0fa8702caabe8fb2
20018040 <Jeff.kim01@student.csulb.edu>
2026-01-02 03:01:07 -0800

    Message and container

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Home from "./sections/Home";
 import Resume from "./sections/Resume";
+import Aboutme from "./sections/Aboutme";
 function App() {
   return (
     <>
       <Home />,
+      <Aboutme />,
       <Resume />;
     </>
   );
---
+++ src/sections/Aboutme.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Message from "../components/Message";
+
+const Aboutme = () => {
+  const title = "About Me\n\n";
+  const firstMessage = `Hi, my name is Jeff Kim and I'm a graduate of CSULB with a B.S degree in Computer Science.\n\nI'm currently looking for a job in the tech field, such as SWE, front-end developer, IT support/operation.
+\nCurrently, I'm learning more about front-end development like JavaScript, React, HTML, CSS.\n
+My strengths include being open to learning new frameworks, languages, and APIs to reach and achieve goals.\n
+Hobbies: body-building, table tennis, basketball, gaming.\n
+You can always reach out to me on my LinkedIn, Email, or GitHub.`;
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        width: "65vw",
+        flexDirection: "column",
+        paddingLeft: "200px",
+        whiteSpace: "pre-line",
+      }}
+    >
+      <div>
+        <Message welcome={title} size={36} color={"#e9e9e9ff"} />
+        <Message welcome={firstMessage} size={36} color={"#e9e9e9ff"} />
+      </div>
+    </div>
+  );
+};
+
+export default Aboutme;
